000669561b2a2a34457a444666bdbd9f951fa9fb
Josue Carrasco Mayhuire <45372826+josh002@users.noreply.github.com>
2021-05-28 20:29:10 -0300

    responsive y descarga de imagen qr generada

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -9,15 +9,32 @@ import { QRCodeComponent } from 'angularx-qrcode';
 export class MainComponent implements OnInit {
   @ViewChild('qrcode') qrcode: HTMLElement;
   public myAngularxQrCode: string = 'https://www.instagram.com/generaci.on/';
-  qrToCreate:string;
+  qrToCreate: string;
+  imgCreated: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  createQR(){
-    if(this.qrToCreate && this.qrToCreate.trim() != ''){
+  async createQR() {
+    if (this.qrToCreate && this.qrToCreate.trim() != '') {
       this.myAngularxQrCode = this.qrToCreate.trim();
+      const link = document.querySelector("a")
+      const img = document.querySelector("img")
+      const imgBase64 = img.src.split(';base64,')[1]
+      const byteCharacters = atob(imgBase64)
+      const byteNumbers = new Array(byteCharacters.length);
+      for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      const blob = new Blob([byteArray], { type: "image/png" });
+      console.log(blob);
+
+      const imgUrl = URL.createObjectURL(blob)
+      link.href = imgUrl;
+      link.download = "img qr";
+      this.imgCreated = true;
     }
   }
 }
